884fb1c5edeef2991b2da6de86fd55657c99f623
skupadhyay024 <connect.shashik@gmail.com>
2019-06-25 20:33:45 +0530

    Change cors 25-06-2019

--- src/main/java/com/students/config/WebConfig.java
+++ src/main/java/com/students/config/WebConfig.java
@@ -1,16 +1,15 @@
-/*
- * package com.students.config;
- * 
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.web.servlet.config.annotation.CorsRegistry; import
- * org.springframework.web.servlet.config.annotation.EnableWebMvc; import
- * org.springframework.web.servlet.config.annotation.WebMvcConfigurer; //Enable
- * when application is used with ui
- * 
- * @Configuration
- * 
- * @EnableWebMvc public class WebConfig implements WebMvcConfigurer {
- * 
- * @Override public void addCorsMappings(CorsRegistry registry) {
- * registry.addMapping("/**"); } }
- */
+
+  package com.students.config;
+  
+  import org.springframework.context.annotation.Configuration; import
+  org.springframework.web.servlet.config.annotation.CorsRegistry; import
+  org.springframework.web.servlet.config.annotation.EnableWebMvc; import
+  org.springframework.web.servlet.config.annotation.WebMvcConfigurer; //Enable while using with ui
+  
+  @Configuration
+  
+  @EnableWebMvc public class WebConfig implements WebMvcConfigurer {
+  
+  @Override public void addCorsMappings(CorsRegistry registry) {
+  registry.addMapping("/**"); } }
+ 
